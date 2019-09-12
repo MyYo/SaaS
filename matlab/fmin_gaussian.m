e9c8719899858dbf92d9b6a0fe424788d1122501
connie_kang <ckyj@stanford.edu>
2019-09-12 13:46:09 -0700

    changing computers

---
+++ matlab/fmin_gaussian.m
@@ -0,0 +1,31 @@
+function coeffs = fmin_gaussian(I, downsample_factor)
+
+n = length(I);
+if downsample_factor > 1
+    n = n/downsample_factor;
+    I_ds = zeros(n, n);
+    for i = 1:n
+        for j = 1:n
+            I_ds(i, j) = I(downsample_factor*i, downsample_factor*j);
+        end
+    end
+    I = I_ds;
+end
+x = 1:n;
+y = 1:n;
+[xx,yy] = meshgrid(x,y);
+
+f = @(a) ((a(4)-a(5))*exp(-((xx-a(1)).^2+(yy-a(2)).^2)/(2*a(3)^2))+a(5));
+persistent aPrev;
+if isempty(aPrev)
+    i = find(I(:)==max(I(:)),1,'first');
+    aInitial = [xx(i),yy(i), n/2, max(I(:)),min(I(:))];
+else
+    aInitial = aPrev;
+end
+
+aBest = fminsearch(@(a)( sum(sum((f(a)-I).^2))),aInitial);
+aPrev = aBest;
+coeffs = aBest.*downsample_factor;
+
+end
